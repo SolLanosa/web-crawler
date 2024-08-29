cd0c041c55053e2b648af6840751d1d1e75da47c
Sol Lanosa <mslanosa@hotmail.com>
2024-08-29 15:15:21 +0200

    feature(service): cors install and config

--- back/src/index.ts
+++ back/src/index.ts
@@ -2,9 +2,11 @@ import express, { Request, Response } from "express";
 import { CONFIG } from "./config";
 import { crawlYCombinatorNews } from "./service";
 import { convertValueToSortAndFilterEnum } from "./utils";
-
+import cors from "cors";
 const app = express();
 const port = CONFIG.PORT || 8080;
+
+app.use(cors());
 
 app.get("/", async (req: Request, res: Response) => {
   const sortAndFilter = convertValueToSortAndFilterEnum(
